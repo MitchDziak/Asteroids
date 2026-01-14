e7073d55a0a3a76aee567e3a804145aa97538f96
MitchDziak <mitchdziak@gmail.com>
2026-01-14 15:04:37 -0800

    updated main.py to restrict framerate to 60

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ from logger import log_state
 
 def main():
     pygame.init()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))  # Remove size= ? Or make [] ? 
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
+
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:  # allows x'ing out of the program
                 return
-        screen.fill(color=(0, 0, 0))
+            
+        screen.fill(color=(0, 0, 0))  # sets screen fill to black
         pygame.display.flip()
+
+        dt = (clock.tick(60)) / 1000  # limits framerate to 60FPS
+
 
 
 if __name__ == "__main__":
